cc255bb5c9477315e845477c7d3764b7df9527f7
Kirill Osterov <43790130+Ziviy@users.noreply.github.com>
2023-02-27 00:53:43 +0300

    Some work on css and logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 function add(num1, num2) {
-    return num1 + num2
+    if (lastResult == 0)
+        return num1 + num2;
+    else {
+        num
+    }
 }
 
 function substract(num1, num2) {
@@ -31,11 +35,45 @@ function operate(operator, num1, num2) {
     }
 }
 
-function handleClick(e) {
-    lastDigit = e.target.innerText;
-        display.innerText += e.target.innerText;
+function refreshDisplay() {
+    display.innerText = lastResult;
+    resultDisplay.innerText = displayValue;
+    displayValue = "";
 }
-const display = document.querySelector(".display");
+
+function updateDisplay() {
+    display.innerText = lastResult;
+    resultDisplay.innerText = displayValue;
+}
+
+let num1 = ""
+let num2 = ""
+let operator = ""
+let lastResult = 0
+let displayValue = ""
+function handleClick(e) {
+    lastSymbol = e.target.innerText;
+    
+    console.log(num1);
+    if (isNaN(lastSymbol)) {
+        console.log("Not a number");
+        if (lastSymbol == "=") {
+            lastResult = operate(operator,num1,num2);
+        }
+        else {
+            operator = lastSymbol;
+            updateDisplay();
+        }
+    }
+    else {
+        console.log("Number");
+        displayValue += lastSymbol;
+        display.innerText = displayValue;
+        num1 = num1 + lastSymbol;
+    }
+}
+const display = document.querySelector(".typing");
+const resultDisplay = document.querySelector(".last-result")
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((element) => {
